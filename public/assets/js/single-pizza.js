bd5ea3004a197d574b0c6fb85c075c71af977e7f
Marcus0224 <marcstlouis04@gmail.com>
2022-05-08 10:18:29 -0400

    integrated reply functionality with front end

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -151,6 +151,28 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
+  fetch('/api/comments/${pizzaId}/${commentId}', {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Conetent-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if(!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    response.json();
+  })
+  .then(commentResponse => {
+    console.log(commentResponse);
+    location.reload();
+  })
+  .catch(err => {
+    console.log(err)
+  });
 }
 
 $backBtn.addEventListener('click', function() {
